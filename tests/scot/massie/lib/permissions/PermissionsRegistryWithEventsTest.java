657db3b0c6295116976cba7dacb3325882878ac7
Chris Massie <chris@massie.scot>
2021-10-16 23:52:24 +0100

    Added tests for PermissionsRegistryWithEvents for group events & clearing events.
    Added tests for PermissionsRegistryWithEvents for group-assigning/revoking events & clearing events.

--- tests/scot/massie/lib/permissions/PermissionsRegistryWithEventsTest.java
+++ tests/scot/massie/lib/permissions/PermissionsRegistryWithEventsTest.java
@@ -198,4 +198,230 @@ public class PermissionsRegistryWithEventsTest extends PermissionsRegistryTest
         reg.revokeDefaultPermission("first.second");
         assertThat(happened.get()).withFailMessage("Event didn't fire.").isTrue();
     }
+
+    @Test
+    void events_assignGroupToUser()
+    {
+        PermissionsRegistryWithEvents<String> reg = getNewPermissionsRegistryWithEvents();
+        MutableWrapper<Boolean> happened = new MutableWrapper<>(false);
+
+        reg.groupAssigned.register(args ->
+        {
+            happened.set(true);
+
+            assertThat(args.getTarget()).isSameAs(PermissionsChangedEventTarget.USER);
+            assertThat(args.getUserTargeted()).isEqualTo("doottarget");
+            assertThat(args.getGroupTargetedId()).isNull();
+            assertThat(args.getGroupAssociatedId()).isEqualTo("dootgroup");
+        });
+
+        reg.assignGroupToUser("doottarget", "dootgroup");
+        assertThat(happened.get()).withFailMessage("Event didn't fire.").isTrue();
+    }
+
+    @Test
+    void events_assignGroupToGroup()
+    {
+        PermissionsRegistryWithEvents<String> reg = getNewPermissionsRegistryWithEvents();
+        MutableWrapper<Boolean> happened = new MutableWrapper<>(false);
+
+        reg.groupAssigned.register(args ->
+        {
+            happened.set(true);
+
+            assertThat(args.getTarget()).isSameAs(PermissionsChangedEventTarget.GROUP);
+            assertThat(args.getUserTargeted()).isNull();
+            assertThat(args.getGroupTargetedId()).isEqualTo("doottarget");
+            assertThat(args.getGroupAssociatedId()).isEqualTo("dootgroup");
+        });
+
+        reg.assignGroupToGroup("doottarget", "dootgroup");
+        assertThat(happened.get()).withFailMessage("Event didn't fire.").isTrue();
+    }
+
+    @Test
+    void events_assignDefaultGroup()
+    {
+        PermissionsRegistryWithEvents<String> reg = getNewPermissionsRegistryWithEvents();
+        MutableWrapper<Boolean> happened = new MutableWrapper<>(false);
+
+        reg.groupAssigned.register(args ->
+        {
+            happened.set(true);
+
+            assertThat(args.getTarget()).isSameAs(PermissionsChangedEventTarget.DEFAULT_PERMISSIONS);
+            assertThat(args.getUserTargeted()).isNull();
+            assertThat(args.getGroupTargetedId()).isNull();
+            assertThat(args.getGroupAssociatedId()).isEqualTo("dootgroup");
+        });
+
+        reg.assignDefaultGroup("dootgroup");
+        assertThat(happened.get()).withFailMessage("Event didn't fire.").isTrue();
+    }
+
+    @Test
+    void events_revokeGroupFromUser_groupPresent()
+    {
+        PermissionsRegistryWithEvents<String> reg = getNewPermissionsRegistryWithEvents();
+        MutableWrapper<Boolean> happened = new MutableWrapper<>(false);
+        reg.assignGroupToUser("doottarget", "dootgroup");
+
+        reg.groupRevoked.register(args ->
+        {
+            happened.set(true);
+
+            assertThat(args.getTarget()).isSameAs(PermissionsChangedEventTarget.USER);
+            assertThat(args.getUserTargeted()).isEqualTo("doottarget");
+            assertThat(args.getGroupTargetedId()).isNull();
+            assertThat(args.getGroupAssociatedId()).isEqualTo("dootgroup");
+        });
+
+        reg.revokeGroupFromUser("doottarget", "dootgroup");
+        assertThat(happened.get()).withFailMessage("Event didn't fire.").isTrue();
+    }
+
+    @Test
+    void events_revokeGroupFromUser_groupNotPresent()
+    {
+        PermissionsRegistryWithEvents<String> reg = getNewPermissionsRegistryWithEvents();
+        MutableWrapper<Boolean> happened = new MutableWrapper<>(false);
+
+        reg.groupRevoked.register(args ->
+        {
+            happened.set(true);
+
+            assertThat(args.getTarget()).isSameAs(PermissionsChangedEventTarget.USER);
+            assertThat(args.getUserTargeted()).isEqualTo("doottarget");
+            assertThat(args.getGroupTargetedId()).isNull();
+            assertThat(args.getGroupAssociatedId()).isEqualTo("dootgroup");
+        });
+
+        reg.revokeGroupFromUser("doottarget", "dootgroup");
+        assertThat(happened.get()).withFailMessage("Event didn't fire.").isTrue();
+    }
+
+    @Test
+    void events_revokeGroupFromGroup_groupPresent()
+    {
+        PermissionsRegistryWithEvents<String> reg = getNewPermissionsRegistryWithEvents();
+        MutableWrapper<Boolean> happened = new MutableWrapper<>(false);
+        reg.assignGroupToGroup("doottarget", "dootgroup");
+
+        reg.groupRevoked.register(args ->
+        {
+            happened.set(true);
+
+            assertThat(args.getTarget()).isSameAs(PermissionsChangedEventTarget.GROUP);
+            assertThat(args.getUserTargeted()).isNull();
+            assertThat(args.getGroupTargetedId()).isEqualTo("doottarget");
+            assertThat(args.getGroupAssociatedId()).isEqualTo("dootgroup");
+        });
+
+        reg.revokeGroupFromGroup("doottarget", "dootgroup");
+        assertThat(happened.get()).withFailMessage("Event didn't fire.").isTrue();
+    }
+
+    @Test
+    void events_revokeGroupFromGroup_groupNotPresent()
+    {
+        PermissionsRegistryWithEvents<String> reg = getNewPermissionsRegistryWithEvents();
+        MutableWrapper<Boolean> happened = new MutableWrapper<>(false);
+
+        reg.groupRevoked.register(args ->
+        {
+            happened.set(true);
+
+            assertThat(args.getTarget()).isSameAs(PermissionsChangedEventTarget.GROUP);
+            assertThat(args.getUserTargeted()).isNull();
+            assertThat(args.getGroupTargetedId()).isEqualTo("doottarget");
+            assertThat(args.getGroupAssociatedId()).isEqualTo("dootgroup");
+        });
+
+        reg.revokeGroupFromGroup("doottarget", "dootgroup");
+        assertThat(happened.get()).withFailMessage("Event didn't fire.").isTrue();
+    }
+
+    @Test
+    void events_revokeDefaultGroup_groupPresent()
+    {
+        PermissionsRegistryWithEvents<String> reg = getNewPermissionsRegistryWithEvents();
+        MutableWrapper<Boolean> happened = new MutableWrapper<>(false);
+
+        reg.groupRevoked.register(args ->
+        {
+            happened.set(true);
+
+            assertThat(args.getTarget()).isSameAs(PermissionsChangedEventTarget.DEFAULT_PERMISSIONS);
+            assertThat(args.getUserTargeted()).isNull();
+            assertThat(args.getGroupTargetedId()).isNull();
+            assertThat(args.getGroupAssociatedId()).isEqualTo("dootgroup");
+        });
+
+        reg.revokeDefaultGroup("dootgroup");
+        assertThat(happened.get()).withFailMessage("Event didn't fire.").isTrue();
+    }
+
+    @Test
+    void events_revokeDefaultGroup_groupNotPresent()
+    {
+        PermissionsRegistryWithEvents<String> reg = getNewPermissionsRegistryWithEvents();
+        MutableWrapper<Boolean> happened = new MutableWrapper<>(false);
+
+        reg.groupRevoked.register(args ->
+        {
+            happened.set(true);
+
+            assertThat(args.getTarget()).isSameAs(PermissionsChangedEventTarget.DEFAULT_PERMISSIONS);
+            assertThat(args.getUserTargeted()).isNull();
+            assertThat(args.getGroupTargetedId()).isNull();
+            assertThat(args.getGroupAssociatedId()).isEqualTo("dootgroup");
+        });
+
+        reg.revokeDefaultGroup("dootgroup");
+        assertThat(happened.get()).withFailMessage("Event didn't fire.").isTrue();
+    }
+
+    @Test
+    void events_clear_empty()
+    {
+        PermissionsRegistryWithEvents<String> reg = getNewPermissionsRegistryWithEvents();
+        MutableWrapper<Boolean> happened = new MutableWrapper<>(false);
+
+        reg.cleared.register(args ->
+        {
+            happened.set(true);
+
+            assertThat(args.getTarget()).isSameAs(PermissionsChangedEventTarget.ALL);
+            assertThat(args.getUserTargeted()).isNull();
+            assertThat(args.getGroupTargetedId()).isNull();
+        });
+
+        reg.clear();
+        assertThat(happened.get()).withFailMessage("Event didn't fire.").isTrue();
+    }
+
+    @Test
+    void events_clear_populated()
+    {
+        PermissionsRegistryWithEvents<String> reg = getNewPermissionsRegistryWithEvents();
+        MutableWrapper<Boolean> happened = new MutableWrapper<>(false);
+        reg.assignUserPermission("doot", "first.second.third");
+        reg.assignGroupPermission("nootgroup", "eins.zwei.drei");
+        reg.assignGroupToUser("zoot", "hoot");
+        reg.assignDefaultPermission("uno.dos.tres");
+
+        reg.cleared.register(args ->
+        {
+            happened.set(true);
+
+            assertThat(args.getTarget()).isSameAs(PermissionsChangedEventTarget.ALL);
+            assertThat(args.getUserTargeted()).isNull();
+            assertThat(args.getGroupTargetedId()).isNull();
+        });
+
+        reg.clear();
+        assertThat(happened.get()).withFailMessage("Event didn't fire.").isTrue();
+    }
+
+    // events_load tests here
 }
